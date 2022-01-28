c8a338b1b77d2e14b51606be3b89d4c7814d3581
tiago <tiago@pop-os.localdomain>
2022-01-28 15:58:00 -0300

    improved code, added sobel BW filter

--- kernel_processing.cu
+++ kernel_processing.cu
@@ -34,7 +34,10 @@ void k_blurChannel (unsigned char * input_channel, unsigned char * outputchannel
 }
 
 __global__
-void k_sobel (unsigned char * input_channel, unsigned char * outputchannel, int numRows, int numCols) {
+void k_sobel (
+            unsigned char * r_in, unsigned char * g_in, unsigned char * b_in,
+            unsigned char * r_out, unsigned char * g_out, unsigned char * b_out,
+            int numRows, int numCols) {
     
     int tIdx = threadIdx.x + blockIdx.x * blockDim.x;
     int tIdy = threadIdx.y + blockIdx.y * blockDim.y;
@@ -43,40 +46,144 @@ void k_sobel (unsigned char * input_channel, unsigned char * outputchannel, int 
 
     if ( index >= numRows * numCols ) return;
 
-    int v_kernel = (
-        input_channel[index] * 0.0 +
+    int v_Rkernel = (
+        r_in[index] * 0.0 +
                 
-        input_channel[index - 1] * 2.0 +     // west
-        input_channel[index + 1] * -2.0 +     // east
+        r_in[index - 1] * 2.0 +     // west
+        r_in[index + 1] * -2.0 +     // east
         
-        input_channel[index - numCols] * 0.0 +    // north
-        input_channel[index + numCols] * 0.0 +    // south
+        r_in[index - numCols] * 0.0 +    // north
+        r_in[index + numCols] * 0.0 +    // south
         
-        input_channel[index - numCols - 1] * 1.0 +    // northwest
-        input_channel[index - numCols + 1] * -1.0 +    // northeast
-        input_channel[index + numCols - 1] * 1.0 +    // southwest
-        input_channel[index + numCols + 1] * -1.0 ) ;   // southeast
+        r_in[index - numCols - 1] * 1.0 +    // northwest
+        r_in[index - numCols + 1] * -1.0 +    // northeast
+        r_in[index + numCols - 1] * 1.0 +    // southwest
+        r_in[index + numCols + 1] * -1.0 ) ;   // southeast
 
-    int h_kernel = (
-        input_channel[index] * 0.0 +
+    int h_Rkernel = (
+        r_in[index] * 0.0 +
                 
-        input_channel[index - 1] * 0.0 +     // west
-        input_channel[index + 1] * 0.0 +     // east
+        r_in[index - 1] * 0.0 +     // west
+        r_in[index + 1] * 0.0 +     // east
         
-        input_channel[index - numCols] * 2.0 +    // north
-        input_channel[index + numCols] * -2.0 +    // south
+        r_in[index - numCols] * 2.0 +    // north
+        r_in[index + numCols] * -2.0 +    // south
         
-        input_channel[index - numCols - 1] * 1.0 +    // northwest
-        input_channel[index - numCols + 1] * 1.0 +    // northeast
-        input_channel[index + numCols - 1] * -1.0 +    // southwest
-        input_channel[index + numCols + 1] * -1.0 ) ;   // southeast
+        r_in[index - numCols - 1] * 1.0 +    // northwest
+        r_in[index - numCols + 1] * 1.0 +    // northeast
+        r_in[index + numCols - 1] * -1.0 +    // southwest
+        r_in[index + numCols + 1] * -1.0 ) ;   // southeast
 
-       outputchannel[index] = (unsigned char) abs( (h_kernel + v_kernel) / 2);
+    r_out[index] = (unsigned char) abs( (h_Rkernel + v_Rkernel) / 2);
+
+    int v_Gkernel = (
+        g_in[index] * 0.0 +
+                
+        g_in[index - 1] * 2.0 +     // west
+        g_in[index + 1] * -2.0 +     // east
+        
+        g_in[index - numCols] * 0.0 +    // north
+        g_in[index + numCols] * 0.0 +    // south
+        
+        g_in[index - numCols - 1] * 1.0 +    // northwest
+        g_in[index - numCols + 1] * -1.0 +    // northeast
+        g_in[index + numCols - 1] * 1.0 +    // southwest
+        g_in[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    int h_Gkernel = (
+        g_in[index] * 0.0 +
+                
+        g_in[index - 1] * 0.0 +     // west
+        g_in[index + 1] * 0.0 +     // east
+        
+        g_in[index - numCols] * 2.0 +    // north
+        g_in[index + numCols] * -2.0 +    // south
+        
+        g_in[index - numCols - 1] * 1.0 +    // northwest
+        g_in[index - numCols + 1] * 1.0 +    // northeast
+        g_in[index + numCols - 1] * -1.0 +    // southwest
+        g_in[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    g_out[index] = (unsigned char) abs( (h_Gkernel + v_Gkernel) / 2);
+
+    int v_Bkernel = (
+        b_in[index] * 0.0 +
+                
+        b_in[index - 1] * 2.0 +     // west
+        b_in[index + 1] * -2.0 +     // east
+        
+        b_in[index - numCols] * 0.0 +    // north
+        b_in[index + numCols] * 0.0 +    // south
+        
+        b_in[index - numCols - 1] * 1.0 +    // northwest
+        b_in[index - numCols + 1] * -1.0 +    // northeast
+        b_in[index + numCols - 1] * 1.0 +    // southwest
+        b_in[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    int h_Bkernel = (
+        b_in[index] * 0.0 +
+                
+        b_in[index - 1] * 0.0 +     // west
+        b_in[index + 1] * 0.0 +     // east
+        
+        b_in[index - numCols] * 2.0 +    // north
+        b_in[index + numCols] * -2.0 +    // south
+        
+        b_in[index - numCols - 1] * 1.0 +    // northwest
+        b_in[index - numCols + 1] * 1.0 +    // northeast
+        b_in[index + numCols - 1] * -1.0 +    // southwest
+        b_in[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    b_out[index] = (unsigned char) abs( (h_Bkernel + v_Bkernel) / 2);
 
 }
 
+__global__
+void k_sobelBW (
+            unsigned char * in_channel, 
+            unsigned char * out_channel,
+            int numRows, int numCols) {
+    
+    int tIdx = threadIdx.x + blockIdx.x * blockDim.x;
+    int tIdy = threadIdx.y + blockIdx.y * blockDim.y;
 
-void cuda_process( unsigned char * d_inR, unsigned char * d_inG, unsigned char * d_inB,
+    int index = numCols * tIdx + tIdy;
+
+    if ( index >= numRows * numCols ) return;
+
+    int v_Rkernel = (
+        in_channel[index] * 0.0 +
+                
+        in_channel[index - 1] * 2.0 +     // west
+        in_channel[index + 1] * -2.0 +     // east
+        
+        in_channel[index - numCols] * 0.0 +    // north
+        in_channel[index + numCols] * 0.0 +    // south
+        
+        in_channel[index - numCols - 1] * 1.0 +    // northwest
+        in_channel[index - numCols + 1] * -1.0 +    // northeast
+        in_channel[index + numCols - 1] * 1.0 +    // southwest
+        in_channel[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    int h_Rkernel = (
+        in_channel[index] * 0.0 +
+                
+        in_channel[index - 1] * 0.0 +     // west
+        in_channel[index + 1] * 0.0 +     // east
+        
+        in_channel[index - numCols] * 2.0 +    // north
+        in_channel[index + numCols] * -2.0 +    // south
+        
+        in_channel[index - numCols - 1] * 1.0 +    // northwest
+        in_channel[index - numCols + 1] * 1.0 +    // northeast
+        in_channel[index + numCols - 1] * -1.0 +    // southwest
+        in_channel[index + numCols + 1] * -1.0 ) ;   // southeast
+
+    out_channel[index] = (unsigned char) abs( (h_Rkernel + v_Rkernel) / 2);
+}
+
+
+void cuda_sobel( unsigned char * d_inR, unsigned char * d_inG, unsigned char * d_inB,
                 unsigned char * d_outR, unsigned char * d_outG, unsigned char * d_outB,
                 int rows, int cols) {
 
@@ -90,13 +197,34 @@ void cuda_process( unsigned char * d_inR, unsigned char * d_inG, unsigned char *
     const dim3 gridSize (blocksX, blocksY, 1);
     
 
-    k_sobel <<< gridSize, blockSize >>> (d_inR, d_outR, rows, cols);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    k_sobel <<< gridSize, blockSize >>> (
+        d_inR, d_inG, d_inB,
+        d_outR, d_outG, d_outB,
+        rows, cols);
+    
+    cudaDeviceSynchronize(); 
+    checkCudaErrors(cudaGetLastError());
+}
 
-    k_sobel <<< gridSize, blockSize >>> (d_inG, d_outG, rows, cols);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+void cuda_sobelBW( 
+                unsigned char * dIn, 
+                unsigned char * dOut, 
+                int rows, int cols) {
 
-    k_sobel <<< gridSize, blockSize >>> (d_inB, d_outB, rows, cols);
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    // wrong blockwidth will cause illegal memory access
 
+    int blockwidth = 16;   
+    int blocksX = rows / blockwidth + 1;    
+    int blocksY = cols / blockwidth + 1;
+    const dim3 blockSize (blockwidth, blockwidth, 1);
+    const dim3 gridSize (blocksX, blocksY, 1);
+    
+
+    k_sobelBW <<< gridSize, blockSize >>> (
+        dIn, 
+        dOut,
+        rows, cols);
+    
+    cudaDeviceSynchronize(); 
+    checkCudaErrors(cudaGetLastError());
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <opencv4/opencv2/core/core.hpp>
 #include <opencv4/opencv2/highgui/highgui.hpp>
 #include <opencv4/opencv2/opencv.hpp>
@@ -12,13 +10,188 @@
 #include <string>
 
 
+void output_image(const std::string &output_file, cv::Mat out_image);
 
-void cuda_process( unsigned char * d_inR, unsigned char * d_inG, unsigned char * d_inB,
+void prepare_allocate1(uchar ** h_channelIn, 
+                      uchar ** d_channelIn,
+                      uchar ** d_channelOut,
+                      int img_size);
+
+void prepare_allocate3(uchar ** h_channelR, uchar ** h_channelG, uchar ** h_channelB, 
+                      uchar ** d_channelR, uchar ** d_channelG, uchar ** d_channelB,
+                      uchar ** d_channelR_out, uchar ** d_channelG_out, uchar ** d_channelB_out,
+                      int img_size);
+
+void cuda_sobel( unsigned char * d_inR, unsigned char * d_inG, unsigned char * d_inB,
                 unsigned char * d_outR, unsigned char * d_outG, unsigned char * d_outB,
                 int rows, int cols);
 
+void sobel(cv::Mat &imgInput, cv::Mat &imgOutput);
 
-void prepare_allocate(uchar ** h_channelR, uchar ** h_channelG, uchar ** h_channelB, 
+void cuda_sobelBW( unsigned char * dIn, unsigned char * dOut, int rows, int cols);
+
+void sobelBW(cv::Mat &imgInput, cv::Mat &imgOutput);
+
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 4) {
+        printf("Usage: process [--filter] inputfile outputname \n");
+        return EXIT_FAILURE;
+    }
+    std::string filter = argv[1];
+    std::string input_file(argv[2]);
+    std::string outputName(argv[argc - 1]);
+    cv::Mat imgInput = cv::imread(input_file.c_str(), cv::IMREAD_REDUCED_COLOR_2);
+    cv::Mat imgOutput;
+
+
+    cv::imshow("INPUT", imgInput);
+
+
+    if (imgInput.empty() ) {
+        std::cerr << "cound not open file: " << input_file << std::endl;
+        exit(1);
+    }
+
+
+    if (filter.compare("--sobel") == 0) {
+        sobel(imgInput, imgOutput);
+    }
+
+    else if (filter.compare("--sobelBW") == 0) {
+        sobelBW(imgInput, imgOutput);
+    } 
+
+    else {
+        std::cerr << "Usage:\n ./process [--sobel, --sobelBW...] input.jpg outputName" << std::endl;
+        exit(1);
+    }
+
+    output_image(outputName, imgOutput);
+
+    return 0;
+}
+
+
+void sobel(cv::Mat &imgInput, cv::Mat &imgOutput) {
+    
+    cv::cvtColor(imgInput, imgInput, cv::COLOR_RGBA2RGB);
+    int img_size = imgInput.rows * imgInput.cols;
+
+    cv::Mat in_rgb[3];
+    cv::Mat out_rgb[3];
+
+    cv::split(imgInput, in_rgb);
+    cv::split(imgInput, out_rgb);
+
+    // h_ refers to data in host (cpu/RAM)
+    // d_ refers to data in compute device (gpu/VRAM)
+
+    uchar * h_channelB_in = in_rgb[2].data;
+    uchar * h_channelG_in = in_rgb[1].data;
+    uchar * h_channelR_in = in_rgb[0].data;
+
+    uchar * h_channelB_out = in_rgb[2].data;
+    uchar * h_channelG_out = in_rgb[1].data;
+    uchar * h_channelR_out = in_rgb[0].data;
+
+    uchar * d_channelB_in;
+    uchar * d_channelG_in;
+    uchar * d_channelR_in;
+
+    uchar * d_channelB_out;
+    uchar * d_channelG_out;
+    uchar * d_channelR_out;
+
+    prepare_allocate3(&h_channelR_in, &h_channelG_in, &h_channelB_in, 
+                     &d_channelR_in, &d_channelG_in, &d_channelB_in,
+                     &d_channelR_out, &d_channelG_out, &d_channelB_out, 
+                     img_size);
+
+    
+    GpuTimer timer;
+
+    timer.Start();
+    cuda_sobel( d_channelR_in, d_channelG_in, d_channelB_in, 
+               d_channelR_out, d_channelG_out, d_channelB_out, 
+               imgInput.rows, imgInput.cols);               
+    timer.Stop();
+
+    printf("elapsed: %f ms\n", timer.Elapsed());
+
+
+    checkCudaErrors( cudaMemcpy( h_channelR_out, d_channelR_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
+    checkCudaErrors( cudaMemcpy( h_channelG_out, d_channelG_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
+    checkCudaErrors( cudaMemcpy( h_channelB_out, d_channelB_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
+
+    cudaDeviceSynchronize(); 
+    checkCudaErrors(cudaGetLastError());
+
+    cudaFree(d_channelB_in);
+    cudaFree(d_channelG_in);
+    cudaFree(d_channelR_in);
+    
+    cudaFree(d_channelR_out);
+    cudaFree(d_channelG_out);
+    cudaFree(d_channelB_out);
+
+    out_rgb[0].data = h_channelR_out; 
+    out_rgb[1].data = h_channelG_out;
+    out_rgb[2].data = h_channelB_out;
+
+    cv::merge(out_rgb, 3, imgOutput);
+}
+
+
+void sobelBW(cv::Mat &imgInput, cv::Mat &imgOutput) {
+    
+    cv::cvtColor(imgInput, imgInput, cv::COLOR_RGBA2GRAY);
+    int img_size = imgInput.rows * imgInput.cols;
+
+    cv::Mat in_rgb[1];
+    cv::Mat out_rgb[1];
+
+    cv::split(imgInput, in_rgb);
+    cv::split(imgInput, out_rgb);
+
+    // h_ refers to data in host (cpu/RAM)
+    // d_ refers to data in compute device (gpu/VRAM)
+
+
+    uchar * h_channelIn = in_rgb[0].data;
+    uchar * h_channelOut = in_rgb[0].data;
+
+    uchar * d_channelIn;
+    uchar * d_channelOut;
+
+    prepare_allocate1(&h_channelIn, &d_channelIn, &d_channelOut, img_size);
+
+    GpuTimer timer;
+
+    timer.Start();
+    cuda_sobelBW( d_channelIn, d_channelOut, imgInput.rows, imgInput.cols);               
+    timer.Stop();
+
+    printf("elapsed: %f ms\n", timer.Elapsed());
+
+    checkCudaErrors( cudaMemcpy( h_channelOut, d_channelOut, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
+
+    cudaDeviceSynchronize(); 
+    checkCudaErrors(cudaGetLastError());
+
+
+    cudaFree(d_channelIn);
+    cudaFree(d_channelOut);
+
+
+    out_rgb[0].data = h_channelOut; 
+
+    cv::merge(out_rgb, 1, imgOutput);
+}
+
+
+void prepare_allocate3(uchar ** h_channelR, uchar ** h_channelG, uchar ** h_channelB, 
                       uchar ** d_channelR, uchar ** d_channelG, uchar ** d_channelB,
                       uchar ** d_channelR_out, uchar ** d_channelG_out, uchar ** d_channelB_out,
                       int img_size)
@@ -42,6 +215,21 @@ void prepare_allocate(uchar ** h_channelR, uchar ** h_channelG, uchar ** h_chann
 
 }
 
+
+void prepare_allocate1(uchar ** h_channelIn, 
+                      uchar ** d_channelIn,
+                      uchar ** d_channelOut,
+                      int img_size)
+{
+    checkCudaErrors(cudaMalloc(d_channelIn, sizeof(uchar) * img_size));
+
+    checkCudaErrors(cudaMalloc(d_channelOut, sizeof(uchar) * img_size));
+
+    checkCudaErrors(cudaMemcpy( *d_channelIn, *h_channelIn, sizeof(uchar) * img_size, cudaMemcpyHostToDevice));
+
+    checkCudaErrors(cudaMemset(*d_channelOut, 0, sizeof(uchar) * img_size));
+}
+
 void output_image(const std::string &output_file, cv::Mat out_image) {
 
     cv::imshow("OUTPUT", out_image);
@@ -52,98 +240,5 @@ void output_image(const std::string &output_file, cv::Mat out_image) {
     }
     
     // output the image
-    cv::imwrite(output_file.c_str(), out_image);
-}
-
-int main(int argc, char const *argv[])
-{
-    if (argc != 3) {
-        printf("Usage: process inputfile outputname.jpg \n");
-        return EXIT_FAILURE;
-    }
-
-    std::string input_file(argv[1]);
-    std::string output_file(argv[2]);
-    cv::Mat cv_imageInput = cv::imread(input_file.c_str(), cv::IMREAD_REDUCED_COLOR_4);
-    cv::Mat cv_img_output;
-
-
-    cv::imshow("INPUT", cv_imageInput);
-
-
-    if (cv_imageInput.empty() ) {
-        std::cerr << "cound not open file: " << input_file << std::endl;
-        exit(1);
-    }
-
-    // convert RGB to RGBA
-    cv::cvtColor(cv_imageInput, cv_imageInput, cv::COLOR_RGBA2RGB);
-
-    int img_size = cv_imageInput.rows * cv_imageInput.cols;
-
-    cv::Mat in_rgb[3];
-    cv::Mat out_rgb[3];
-
-    cv::split(cv_imageInput, in_rgb);
-    cv::split(cv_imageInput, out_rgb);
-
-    // h_ refers to data/variables in host (cpu/RAM)
-    // d_ refers to data/variables in compute device (gpu/VRAM)
-
-    uchar * h_channelB_in = in_rgb[2].data;
-    uchar * h_channelG_in = in_rgb[1].data;
-    uchar * h_channelR_in = in_rgb[0].data;
-
-    uchar * h_channelB_out = in_rgb[2].data;
-    uchar * h_channelG_out = in_rgb[1].data;
-    uchar * h_channelR_out = in_rgb[0].data;
-
-    uchar * d_channelB_in;
-    uchar * d_channelG_in;
-    uchar * d_channelR_in;
-
-    uchar * d_channelB_out;
-    uchar * d_channelG_out;
-    uchar * d_channelR_out;
-
-    prepare_allocate(&h_channelR_in, &h_channelG_in, &h_channelB_in, 
-                     &d_channelR_in, &d_channelG_in, &d_channelB_in,
-                     &d_channelR_out, &d_channelG_out, &d_channelB_out, 
-                     img_size);
-
-    
-    GpuTimer timer;
-
-    timer.Start();
-    cuda_process( d_channelR_in, d_channelG_in, d_channelB_in, 
-               d_channelR_out, d_channelG_out, d_channelB_out, 
-               cv_imageInput.rows, cv_imageInput.cols);               
-    timer.Stop();
-
-    printf("elapsed: %f ms\n", timer.Elapsed());
-
-
-    checkCudaErrors( cudaMemcpy( h_channelR_out, d_channelR_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
-    checkCudaErrors( cudaMemcpy( h_channelG_out, d_channelG_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
-    checkCudaErrors( cudaMemcpy( h_channelB_out, d_channelB_out, sizeof(uchar) * img_size, cudaMemcpyDeviceToHost) );
-
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-
-    cudaFree(d_channelB_in);
-    cudaFree(d_channelG_in);
-    cudaFree(d_channelR_in);
-    
-    cudaFree(d_channelR_out);
-    cudaFree(d_channelG_out);
-    cudaFree(d_channelB_out);
-
-    out_rgb[0].data = h_channelR_out; 
-    out_rgb[1].data = h_channelG_out;
-    out_rgb[2].data = h_channelB_out;
-
-    cv::merge(out_rgb, 3, cv_img_output);
-
-    output_image(output_file, cv_img_output);
-
-    return 0;
+    cv::imwrite(output_file + ".jpg", out_image);
 }
