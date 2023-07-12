77f34055935d104892e993a769ca6fc0ed992ea7
Tiago Orlando <tiago_orlando@yahoo.com>
2023-07-12 16:48:28 -0300

    refactor: added typedef for unsigned char

--- src/cuda/memory.cuh
+++ src/cuda/memory.cuh
@@ -2,18 +2,19 @@
 #define MEMORY_CLASS_H
 
 #include "../utils/utils.h"
+typedef unsigned char uchar;
 
 class Memory
 {
     public:
-        static void prepare_allocate1(unsigned char ** h_channelIn, 
-                        unsigned char ** d_channelIn,
-                        unsigned char ** d_channelOut,
+        static void prepare_allocate1(uchar ** h_channelIn, 
+                        uchar ** d_channelIn,
+                        uchar ** d_channelOut,
                         int img_size);
 
-        static void prepare_allocate3(unsigned char ** h_channelR, unsigned char ** h_channelG, unsigned char ** h_channelB, 
-                            unsigned char ** d_channelR, unsigned char ** d_channelG, unsigned char ** d_channelB,
-                            unsigned char ** d_channelR_out, unsigned char ** d_channelG_out, unsigned char ** d_channelB_out,
+        static void prepare_allocate3(uchar ** h_channelR, uchar ** h_channelG, uchar ** h_channelB, 
+                            uchar ** d_channelR, uchar ** d_channelG, uchar ** d_channelB,
+                            uchar ** d_channelR_out, uchar ** d_channelG_out, uchar ** d_channelB_out,
                             int img_size);
 };
 
